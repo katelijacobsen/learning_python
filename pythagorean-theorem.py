b35ab5986d285a11d5f10a7139008eb6a1a76f42
katelijacobsen <katjamaehleke98@gmail.com>
2025-11-06 10:37:05 +0100

    Push

---
+++ pythagorean-theorem.py
@@ -0,0 +1,21 @@
+# User input
+# Python uses the input() function to get the users input. It can be used for almost every popular thing such as website, mobile, app or video game nowadays.
+
+# username = input("What's your name?")
+
+# int()
+# another function that can be wrapped around the input() to convert the text string into a number
+
+# age = int(input('Your age?'))
+
+# Task
+#Create a hypotenuse.py program that asks
+#the user for two numbers, a and b, and then calculates the hypotenuse c
+# Note: To make the square root, we will have to wrap the variables in parentese and add 0.5 as an exponent (**)
+
+a = 5
+b = 8
+
+c = (a**2 + b**2)**0.5
+
+print(c)
