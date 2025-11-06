e94a526396a57b13ba009249bb888b17b1fbf471
katelijacobsen <katjamaehleke98@gmail.com>
2025-11-06 09:39:10 +0100

    Types of Variables in Python

--- createcode.py
+++ createcode.py
@@ -1,3 +1,16 @@
+# Data Types 
+# We use variables to store data values. Each variable holds a name and holds a value. 
+# It can be compared to a slap on a label, and it can store stuff.
 
+# For example, similar to JavaScript: 
 
-print('Hello World')
+name = 'Earl Bachman'  # string value. Its simple and stores text. Can either be wrapped in " " or ''. 
+user_id = 12345678 #Integer (int), is a whole number. It doesn't use decimal numbers and contains the number 0 (positive and negative)
+# As an example, we could use this type of variable such as counting or the number amount of how many balls are in a jar. 
+progress = 0.67 # This variable is called a float-point number (float), which uses a decimal number. Can be used for measurements or representing fractions.
+#Good for using percentage, calculations or measuring sizes of objects.
+xp = 20
+verified = True #Boolean what can be toggled between true or false, there is nothing in between. See it as black and white.
+#This value is capitalized in Python. Fun fact: its named after the british mathematician George Boole (hehehe)
+
+print(name, user_id, verified)
