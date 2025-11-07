d0e012bd4c32364c975c80e4fbe8abdd021210bb
katelijacobsen <katjamaehleke98@gmail.com>
2025-11-07 13:28:17 +0100

    push exercise

--- conditions.py
+++ conditions.py
@@ -4,3 +4,14 @@ if name == "John":
     print('Kæft det grimt...')
 else:
     print("Ejjj det et pændt navn")
+
+tal = int(input('Indtast et tal:    '))
+
+if (tal >= 100): 
+    print("Nøj, et stort tal du fandt der")
+elif tal < 20:
+    print('Godt nok et lille tal')
+elif 20 < tal and tal < 80:
+    print('mellemstort tal')
+else:
+    print('hvAbehar')
