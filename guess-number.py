94377baba8c667af1e9659969f291dd76a554dc9
katelijacobsen <katjamaehleke98@gmail.com>
2025-11-11 18:26:46 +0100

    structure variables

--- guess-number.py
+++ guess-number.py
@@ -1,9 +1,11 @@
 from random import randint
 
 
-random_number = randint(1,5)
+guesses = 1
 
+random_number = randint(1,5)
 user_number = int(input("Gæt et tal:   "))
+
 
 while True:
     if user_number < random_number:
@@ -12,5 +14,7 @@ while True:
         print("For højt")
     else:
         print("Yes you guessed it!")
+        print(f"Du brugte {guesses} forsøg")
         break
     user_number = int(input("Prøv igen:     "))
+    guesses += 1
